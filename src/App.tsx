e05c8528b11de9c9b94d8a402a7f08b0c30366c4
Victor Hernandez, Jr <vh4697@gmail.com>
2024-01-19 20:34:24 -0600

    initial foundation

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import './App.css'
 
 function App() {
 
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <div className='d-flex flex-column justify-content-center align-items-center'>
+            <div className='my-4 d-flex flex-column align-items-center'>
+                <h1 className='text-center text-dark'>Pok&eacute;mon Generator</h1>
+                <a className='btn btn-primary'>Generate</a>
+            </div>
+            <div className='info-box p-3 border border-dark rounded bg-light d-flex flex-column justify-content-center align-items-center'>
+                <span>Pokemon Name</span>
+                <img src='/' alt='image of pokemon'></img>
+            </div>
+        </div>
+    )
 }
 
 export default App
