4dd9d27d239b5a7a1061ddda8cda6c8efbe995f7
Victor Hernandez, Jr <vh4697@gmail.com>
2024-01-21 16:03:20 -0600

    added Hading component

---
+++ src/components/Heading.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react"
+
+interface Props {
+    children: ReactNode;
+}
+
+function Heading({ children }: Props) {
+    return (
+        <h1 className='text-dark'>
+            <img src='src/assets/pokeball.png' width='50' className='mb-1 mx-1' />
+            {children}
+        </h1>
+    )
+}
+
+export default Heading
